f6e6d8b58cec8149e33435772c317f6af2b6439c
ChrxnZX <105559308+ChrxnZ@users.noreply.github.com>
2022-08-25 18:03:09 +0400

    Update Online_Arm64Asm_To_Hex.lua

--- Online_Arm64Asm_To_Hex.lua
+++ Online_Arm64Asm_To_Hex.lua
@@ -1,6 +1,7 @@
 local cho = gg.choice
 local pr = gg.prompt
 local mr = gg.makeRequest
+local mcho = gg.multiChoice
 
 
 main = cho({
@@ -19,8 +20,18 @@ if main == 1 then
     ["content-type"] = "application/json"
     },  '{"asm": "' .. asm_args[1] .. '"}')
   local hex = tostring(response["content"]:match([[arm64.+true, ".+},]])):match([["%d+.+%d]]):match("%d+.+%d"):gsub([[\n]], "")
+  
+  choice = mcho({
+  	"Inputted Arm",
+  	"Converted Hex"
+  }, nil, "Choose What To Display:")
+  
+  if choice[1] == true then
   print("Arm64 Assembly: " .. asm_args[1] .. "")
+  end
+  if choice[2] == true then
   print("Hex: " .. hex .. "")
+end
 end
 
 if main == 2 then
